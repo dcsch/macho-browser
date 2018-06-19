d26cbdc7f04bb3bc7a846c05f495a19aa8ff8794
David Schweinsberg <david.schweinsberg@gmail.com>
2018-06-19 22:27:08 +1000

    Added test stub

---
+++ MachOBrowserTests/MachOBrowserTests.m
@@ -0,0 +1,33 @@
+//
+//  MachOBrowserTests.m
+//  MachOBrowserTests
+//
+//  Created by David Schweinsberg on 6/19/18.
+//
+
+#import <XCTest/XCTest.h>
+#import "MachObject.h"
+
+@interface MachOBrowserTests : XCTestCase
+
+@end
+
+@implementation MachOBrowserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testMachObjectLoad {
+    NSData *data = [NSData dataWithContentsOfURL:NSBundle.mainBundle.executableURL];
+    MachObject *mo = [[MachObject alloc] initWithData:data];
+    XCTAssertEqual(mo.magic, MH_MAGIC_64);
+}
+
+@end
